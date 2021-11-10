94788bcde167c817151e0edfafde357f73c25763
Larisa Antokhina <laranto1985@gmail.com>
2021-11-10 11:54:26 +0300

    add create, edit routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
   root "pages#home"
   get 'about', to: "pages#about"
 
